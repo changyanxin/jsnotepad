61d451b044ffb9233ef9f28b386511c8420896ae
常延欣 <33642984+changyanxin@users.noreply.github.com>
2020-04-10 20:28:42 +0800

    Update menubar.js

--- com/menubar/menubar.js
+++ com/menubar/menubar.js
@@ -1,26 +1,17 @@
 function Menubar() {
-  // 元素
   this.$menubar = null;
   this.$menus = [];
-
-  // 菜单显示状态
   this.openMenu = -1;
-
-  // 初始化
   this.init = function (menuData) {
-    // 添加元素
     var $menubar = $('<div class="menubar"></div>');
-    // 添加 menu-title
     var $menu_title = $('<ul class="menu_title">');
     for (var i = 0; i < menuData.length; i++) {
       var $li = $(`<li>${menuData[i].title}</li>`);
-      // 绑定点击事件和滑动事件
       $li.click(this.showMenu.bind(this, i));
       $li.mouseover(this.showMenu.bind(this, i));
       $menu_title.append($li);
     }
     $menubar.append($menu_title);
-    // 添加menus
     for (var i = 0; i < menuData.length; i++) {
       var $menu = $('<ul class="menu"></ul>');
       for (var j = 0; j < menuData[i].menuItems.length; j++) {
@@ -28,7 +19,6 @@ function Menubar() {
           var $menu_item = $('<li class="menu_hr"></li>');
         } else {
           var $menu_item = $(`<li class="menu_item">${menuData[i].menuItems[j].title}<span class="shortcut">${menuData[i].menuItems[j].shortcut}</span></li>`);
-          // 绑定点击事件
           $menu_item.click(menuData[i].menuItems[j].handler);
         }
         $menu.append($menu_item);
@@ -41,7 +31,6 @@ function Menubar() {
     this.$menubar = $menubar;
     return $menubar;
   };
-  // 显示第 index 个菜单
   this.showMenu = function (index, e) {
     e.stopPropagation();
     if (e.type == 'click' && this.openMenu > -1) {
@@ -57,7 +46,7 @@ function Menubar() {
       }
     }
   };
-  // 隐藏菜单
+
   this.hideMenu = function () {
     this.openMenu = -1;
     for (var i = 0; i < this.$menus.length; i++) {
